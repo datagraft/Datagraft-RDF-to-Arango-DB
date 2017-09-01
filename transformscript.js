8d8109b462b4128f3f6c76b34b0e240cc3c87371
Simen Samuelsen <simen.samuelsen@gmail.com>
2017-09-01 10:17:14 +0200

    started converting service to use csv parser

--- transformscript.js
+++ transformscript.js
@@ -6,6 +6,7 @@ const jsonParser = bodyParser.json();
 const logging = require('./logging');
 const multer  = require('multer');
 const dl = require('datalib');
+const pap = require('./papaparse.min.js');
 const lineReader = require('line-reader');
 const fs = require('fs');
 
@@ -136,233 +137,213 @@ module.exports = (app) => {
         }
 
         var buffer;
-    
-        fs.readFile(path, "utf-8", (err, data) => {
-            if (err) {
-                console.log("err" + err + "err");
-                //showAndLogError(res, 400, 'Error reading file');
-                return;
-            }
-            /*
-            buffer = dl.csv(data);
-            
-            headings = []; //reset headings
-            
-            for (var i = 0; i < buffer.columns.length; i++) { //for all elements i colums array from datalib parse
-                var value = buffer.columns[i];
-                headings[value] = i; //add an entry with value as key and arrayposition as value
-            } */
-            
-            buffer = data.split('\n');
 
-            
-            //Make arrays of each line
-            for (var i = 0; i < buffer.length; i++) {
-                buffer[i] = buffer[i].split(',');
-                
-                var buffer2 = [];
-                var string = "";
-                var makingstring = false;
-                
-                for(var j = 0; j < buffer[i].length; j++){
-                    if(!makingstring){
-                        if(buffer[i][j].charAt(0) == '"'){
-                            makingstring = true;
-                        }else{
-                            buffer2.push(buffer[i][j]);
-                        }
-                    }
-                    
-                    if(buffer[i][j].charAt(buffer[i][j].length-1) == '"'){
-                        string += buffer[i][j];
-                        buffer2.push(string.substring(1, string.length -1));
-                            
-                        string = "";
-                        makingstring = false;
-                    }else if(makingstring){
-                        string += buffer[i][j];
-                    }
+       /* console.log(typeof req.files.csv[0]);
+        pap.parse(req.files.csv[0], {
+            worker: false,
+            step: function(row){
+                console.log("Row " + row.data);
+            },
+            complete: function() {
+                console.log("All done!");
+            }
+        });*/
+        var count = 0;
+        var arango_value = [];  //main value array
+        var arango_value2 = {}; //array for namepsace nodes
+        var arango_edge = [];   //array for all edges
+        var head = false;
+        headings = [];
+
+        lineReader.eachLine(path, function(row, last) {
+            row = pap.parse(row);
+            if(row.errors.length > 0){
+                console.log("err " + row.errors);
+                return row.errors;
+            }
+
+            if(head == false){
+                head = true;
+                console.log(row.data[0].length);
+                for (var i = 0; i < row.data[0].length; i++) { //for all elements i colums array from datalib parse
+                    var value = row.data[0][i];
+                    headings[value] = i; //add an entry with value as key and arrayposition as value
                 }
-                
-                buffer[i] = buffer2;
-            }
+                console.log(headings);
+            } else {
 
-            headings = []; //reset headings
-            for (var i = 0; i < buffer[0].length; i++) { //for all elements i first buffer row
-                var value = buffer[0][i];
-                headings[value] = i; //add an entry with value as key and arrayposition as value
-            }
-            
-            var count = 0;
-            var arango_value = [];  //main value array
-            var arango_value2 = {}; //array for namepsace nodes
-            var arango_edge = [];   //array for all edges
-            
-            var head = headings;
-            
-    
-            function loop(mapping, line){            
-        var obj = {}
-    
-        if(mapping !== null){
-            obj = {"_key":count.toString()};
-            obj['namespace+id'] = "";
-            
-            count ++;
-        }
-        
-        for(key in mapping){
-            
-            //console.log(key);
-            
-            if(key === "prefix"){
-                obj['namespace'] = vocab[mapping[key]].namespace;
-                                
-                var nameHash = obj.namespace.hashCode().toString();
-                var isThere = false;
+                console.log(row.data[0])
+
+                function loop(data, line){            
+                    var obj = {}
                 
-                if(arango_value2.length !== 0){
-                    for(key2 in arango_value2){
-                        if(key2 == nameHash){
-                            isThere = true;
-                        }
+                    if(data !== null){
+                        obj = {"_key":count.toString()};
+                        obj['namespace+id'] = "";
+                        //console.log(obj._key);
+                        count ++;
                     }
-                }
-                
-                if(!isThere && nameHash !== undefined){
-                    arango_value2[nameHash] = {"_key": nameHash, "label": obj.namespace};
-                    arango_edge.push({"_from": 0, "_to": nameHash});
             
-                }
-                
-                obj['namespace+id'] = vocab[mapping[key]].namespace;                
-            }
-            
-            if(key === 'constant' || key === "propertyName"){
-                obj['namespace+id'] += mapping[key];                
-            }
-            
-            if(key === "column" || key === "literalValue"){
-                var test = mapping[key].value;
-                var field = head[test];
-                var val = line[field];
-
-                obj[key] = mapping[key].value;
-                obj['value'] = line[field];
-
-                if(!isNaN(obj.value)){
-                    obj.value = +obj.value;
-                }   
-
-            }else if(Array.isArray(mapping[key])){
-                mapping[key].forEach(function(entry) {
-                    var ArrToObj = {"_from": obj._key, "_to":loop(entry, line)};
-                    if(typeof ArrToObj._to != 'undefined'){
-                        arango_edge.push(ArrToObj);
-                    }
-                });
-            }else if(typeof mapping[key] === 'object'){
-                var ObjToObj = {"_from": obj._key, "_to":loop(mapping[key], line)};
-                if(typeof ObjToObj._to != 'undefined'){
-                    arango_edge.push(ObjToObj);
-                }
-            }else{
-                obj[key] = mapping[key];
-            }
-        }
-        
-        if(mapping !== null){
-            obj['label'] = "";
-            
-            if(typeof obj.prefix != 'undefined'){
-                obj.label = obj.label + obj.prefix + " ";
-            }
-            
-            if(typeof obj.value != 'undefined'){
-                obj.label = obj.label + obj.value;
-            }
-            
-            if(typeof obj.propertyName != 'undefined'){
-                obj.label = obj.label + ": " + obj.propertyName;
-            }    
-            
-            //handle prefix mapping of keys here.....
-            
-            if(obj.__type !== "Property"){
-                
-                if(obj.value !== undefined){
-                    obj['namespace+id'] += obj.value.toString();
-                    obj['old_key'] = obj._key;
-                    obj._key = obj["namespace+id"].hashCode().toString();
-                    
-                    for(var i = 0; i < arango_edge.length; i++){
-                        if(arango_edge[i]._from === obj.old_key){
-                            arango_edge[i]._from = obj._key;
-                        }else if(arango_edge[i]._to === obj.old_key){
-                            arango_edge[i]._to = obj._key;
+                    for(key in data){
+                        
+                        if(key == "$$hashKey")
+                        {
+                            //console.log(key);
                         }
                         
+                        if(key === "prefix"){
+                        /*  console.log("------------------")
+                            console.log("get prefix!")
+                            console.log(data[key])
+                            console.log(vocab[data[key]])
+                            console.log(data)
+                            console.log("------------------")
+                            */
+                            obj['namespace'] = vocab[data[key]].namespace;
+                                            
+                            var nameHash = obj.namespace.hashCode().toString();
+                            var isThere = false;
+                            
+                            if(arango_value2.length !== 0){
+                                for(key2 in arango_value2){
+                                    if(key2 == nameHash){
+                                        isThere = true;
+                                    }
+                                }
+                            }
+                            
+                            if(!isThere && nameHash !== undefined){
+                                arango_value2[nameHash] = {"_key": nameHash, "label": obj.namespace};
+                                arango_edge.push({"_from": 0, "_to": nameHash});
+                                //console.log(arango_value2);
+                            }
+                            
+                            obj['namespace+id'] = vocab[data[key]].namespace;                
+                        }
+                        
+                        if(key === 'constant' || key === "propertyName"){
+                            obj['namespace+id'] += data[key];                
+                        }
+                        
+                        if(key === "column" || key === "literalValue"){
+                            var test = data[key].value;
+                            var field = headings[test];
+                            var val = line[field];
+
+                            obj[key] = data[key].value;
+                            obj['value'] = line[field];
+
+                            if(!isNaN(obj.value)){
+                                obj.value = +obj.value;
+                            }   
+
+                        }else if(Array.isArray(data[key])){
+                            data[key].forEach(function(entry) {
+                                var ArrToObj = {"_from": obj._key, "_to":loop(entry, line)};
+                                if(typeof ArrToObj._to != 'undefined'){
+                                    arango_edge.push(ArrToObj);
+                                }
+                            });
+                        }else if(typeof data[key] === 'object'){
+                            var ObjToObj = {"_from": obj._key, "_to":loop(data[key], line)};
+                            if(typeof ObjToObj._to != 'undefined'){
+                                arango_edge.push(ObjToObj);
+                            }
+                        }else{
+                            if(key != "$$hashKey"){
+                                obj[key] = data[key];
+                            }
+                        }
                     }
+                    
+                    if(data !== null){
+                        obj['label'] = "";
+                        
+                        if(typeof obj.prefix != 'undefined'){
+                            obj.label = obj.label + obj.prefix + " ";
+                        }
+                        
+                        if(typeof obj.value != 'undefined'){
+                            obj.label = obj.label + obj.value;
+                        }
+                        
+                        if(typeof obj.propertyName != 'undefined'){
+                            obj.label = obj.label + ": " + obj.propertyName;
+                        }    
+                        
+                        //handle prefix mapping of keys here.....
+                        
+                        if(obj.__type !== "Property"){
+                            
+                            if(obj.value !== undefined){
+                                obj['namespace+id'] += obj.value.toString();
+                                obj['old_key'] = obj._key;
+                                obj._key = obj["namespace+id"].hashCode().toString();
+                                
+                                for(var i = 0; i < arango_edge.length; i++){
+                                    if(arango_edge[i]._from === obj.old_key){
+                                        arango_edge[i]._from = obj._key;
+                                    }else if(arango_edge[i]._to === obj.old_key){
+                                        arango_edge[i]._to = obj._key;
+                                    }
+                                    
+                                }
+                            }
+                            
+                            if(obj.namespace !== undefined){
+                                arango_edge.push({"_from":obj.namespace.hashCode().toString(), "_to":obj._key});
+                            }
+                        }
+                        
+                        //End prefix handling of keys
+                        var existsInSet = false;
+                        
+                        for (key in arango_value){
+                            if(arango_value[key]._key === obj._key){
+                                existsInSet = true;
+                                break;
+                            }
+                        }
+                        
+                        if(!existsInSet){
+                            arango_value.push(obj);
+                        }
+                    }
+                    
+                    return obj._key;
+                };
+            
+                var rootNode = {"_key":count.toString(), "label": "Start node", "value": "Start node"};
+                count ++;
+                
+                for(var i = 1; i < row.data[0].length; i++){
+                //for(var i = 1; i < 3; i++){
+                    arango_edge.push({"_from": 0, "_to":(count).toString()});
+                    obKey = loop(mapping, row.data[0][i]);
                 }
                 
-                if(obj.namespace !== undefined){
-                    arango_edge.push({"_from":obj.namespace.hashCode().toString(), "_to":obj._key});
+                if(last){
+                    //delete rootNode.graphRoots;
+                    arango_value.push(rootNode);
+                            
+                    /*Add all namespaces to node collection*/
+                    for(key in arango_value2){
+                        arango_value.push(arango_value2[key]);
+                    }
+                            
+                    /*Sort the info so it's easier to read*/
+                    arango_value.sort(function(a, b) {
+                        return a._key - b._key;
+                    });
+                            
+                    arango_edge.sort(function(a, b) {
+                        return a._from - b._from;
+                    });
+                    //Save as array with objects
+                    write_object(arango_value, arango_edge);         
+                    res.send('ok');                
                 }
             }
-            
-            //End prefix handling of keys
-            var existsInSet = false;
-            
-            for (key in arango_value){
-                if(arango_value[key]._key === obj._key){
-                    existsInSet = true;
-                    break;
-                }
-            }
-            
-            if(!existsInSet){
-                arango_value.push(obj);
-            }
-        }
-        
-        return obj._key;
-    };
-    
-            var rootNode = {"_key":count.toString(), "label": "Start node", "value": "Start node"};
-            count ++;
-    
-            for(var i = 1; i < buffer.length; i++){
-            //for(var i = 1; i < 3; i++){
-                arango_edge.push({"_from": 0, "_to":(count).toString()});
-                obKey = loop(mapping, buffer[i]);
-            }
-       
-            //delete rootNode.graphRoots;
-            arango_value.push(rootNode);
-            
-            /*Add all namespaces to node collection*/
-            for(key in arango_value2){
-                arango_value.push(arango_value2[key]);
-            }
-            
-            /*Sort the info so it's easier to read*/
-            arango_value.sort(function(a, b) {
-                return a._key - b._key;
-            });
-            
-            arango_edge.sort(function(a, b) {
-                return a._from - b._from;
-            });
-    
-    
-            
-            //Save as array with objects
-            write_object(arango_value, arango_edge);
-                    
-            res.send('ok');     
-        }); 
+        });
     });
 };
-
-
-
