a3c5ef40f1bc5249b406fbe429a2590c2cb7d900
Nikolay Nikolov <nikolay.nikolov@sintef.no>
2018-09-06 17:07:46 +0200

    Added body parser middleware and Dockerfile

--- server.js
+++ server.js
@@ -5,20 +5,19 @@
 var express = require('express')
 var zip = require('express-zip');
 var cors = require('cors')
-    const logging = require('./logging');
+const logging = require('./logging');
 //require('./transformscript');
-
+var bodyParser = require('body-parser');
 const request = require('request');
-
-
 const serverPort = process.env.HTTP_PORT || 3030;
 const resultStorageLocation = process.env.RESULT_STORAGE_LOCATION || './results/';
 const inputStorageLocation = process.env.INPUT_STORAGE_LOCATION || './uploads/';
 
 const app = express();
+app.use(bodyParser.json({limit: '50mb'}));
 
 app.use(cors());
- 
+
 app.get('/', (req, res) => {
   res.send('ok');
 });
